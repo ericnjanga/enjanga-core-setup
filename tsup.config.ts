006b7e89cd8824e9e066e401e0b8b5656bb4d401
Eric Njanga <eric.njanga@gmail.com>
2025-09-17 12:41:40 -0400

    chore: setup tsup file

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,8 +4,24 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts"],
   format: ["esm", "cjs"],
-  dts: true, // generates .d.ts files
+  dts: true,              // generate .d.ts
   outDir: "dist",
-  external: ["react", "react-dom", "@carbon/react", "clsx"],
-  clean: true, // removes old build artifacts before building
+  clean: true,            // clean dist before building
+  external: [
+    "react",
+    "react-dom",
+    "next",
+    "@carbon/react",
+    "@carbon/icons-react",
+    "@carbon/styles",
+    "@carbon/pictograms-react",
+    "carbon-components",
+    "clsx",
+    "axios",
+    "@tanstack/react-query",
+    "@tanstack/react-query-devtools",
+    "@contentful/rich-text-types",
+    "smoothscroll-polyfill",
+    "wicg-inert"
+  ]
 });
