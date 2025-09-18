d3682bdd4af571094c19b49317a4b7bdfbc0d37e
Eric Njanga <eric.njanga@gmail.com>
2025-09-18 08:07:01 -0400

    chore: build

---
+++ dist/next.mjs
@@ -0,0 +1,10 @@
+import "./chunk-R7POPVJR.mjs";
+
+// src/next.ts
+import { default as default2 } from "next/link";
+import { default as default3 } from "next/image";
+export * from "next/navigation";
+export {
+  default3 as Image,
+  default2 as Link
+};
