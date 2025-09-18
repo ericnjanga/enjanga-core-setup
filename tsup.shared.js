2bd1ad3d23f9a7afa2009d88a792e27d01206409
Eric Njanga <eric.njanga@gmail.com>
2025-09-17 21:59:20 -0400

    chore: package update

---
+++ tsup.shared.js
@@ -0,0 +1,20 @@
+// enjanga-core-setup/tsup.shared.js
+module.exports = {
+  externals: [
+    "react",
+    "react-dom",
+    "next",
+    "@carbon/react",
+    "@carbon/icons-react",
+    "@carbon/styles",
+    "@carbon/pictograms-react",
+    "carbon-components",
+    "clsx",
+    "axios",
+    "@tanstack/react-query",
+    "@tanstack/react-query-devtools",
+    "@contentful/rich-text-types",
+    "smoothscroll-polyfill",
+    "wicg-inert"
+  ]
+};
